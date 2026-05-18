3c902d5109cee59d87dae692534930d5c62ec19e
Jj Mensah (s5707298) <s5707298@bournemouth.ac.uk>
2026-05-18 13:48:51 +0100

    Added Prefabs / Scripts

---
+++ Aporia2.0/Assets/Scripts/Gameplay/Player/Health.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int maxHealth;
+    [SerializeField] public int currentHealth;
+
+    [SerializeField] private Slider healthSlider;
+    [SerializeField] private Canvas canvas;
+
+    [SerializeField] private Transform cameraPosition;
+
+    //Events
+
+    public event Action OnDeath;
+
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = currentHealth;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            //Test Damage on Enemy
+            TakeDamage(10);
+        }
+    }
+
+    private void LateUpdate()
+    {
+       canvas.transform.LookAt(cameraPosition);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        Debug.Log($"Before Damage: {currentHealth}");
+
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+
+        Debug.Log($"After Damage: {currentHealth}");
+
+        healthSlider.value = currentHealth;
+
+        if (currentHealth <= 0)
+        {
+            Debug.Log("Enemy died");
+            OnDeath?.Invoke();
+        }
+    }
+
+    public void ResetHealth()
+    {
+        currentHealth = maxHealth;
+
+        healthSlider.value = currentHealth;
+    }
+
+    public void Death()
+    {
+        Debug.Log(gameObject.name + " is dead!");
+
+        Destroy(gameObject);
+    }
+}
